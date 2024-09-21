c0cdbb632507a85fda36c203c3b0d1f5873bdbc6
1korenn <105160059+1korenn@users.noreply.github.com>
2024-09-21 13:35:16 +0000

    new-app

--- kornweb/kornweb/urls.py
+++ kornweb/kornweb/urls.py
@@ -20,5 +20,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("introduction/", include("introduction.urls"))
+    path("introduction/", include("introduction.urls")),
+    path("mybday/", include("mybday.urls"))
 ]
